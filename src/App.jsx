49544f86a1ee8af36473c5d15e7e15731e3fa95c
Anand Patel <ap8821897076@gmail.com>
2024-09-24 22:54:48 +0530

    UseState function example added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 
 const App =  () => {
 
+  const [title,setTitle] = useState("Hello world")
+
+    // let title = "Hello World!"
+
+  const text = () => {
+    setTitle("Hello world! clicked");
+    }
+
+
   return (
-    <div className="w-full h-screen flex items-center justify-center flex-col bg-zinc-600">
-      <h1 className="font-bold text-3xl mb-2 text-xl">Boiler plate set </h1>
-      <h2>{import.meta.env.VITE_API_KEY}</h2>
-    </div>
+     <>
+      <h1 className="text-3xl"> {title} </h1>
+      <button onClick={text} className="py-2 px-4 bg-teal-400 mt-4 text-white">Click Me</button> 
+     </>
   )
 }
 
